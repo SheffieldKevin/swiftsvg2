1dbda168d8922be2dcd0a36e81ef6972a2c2e363
Kevin Meaney <ktam@yvs.eu.com>
2016-04-13 11:00:40 +0100

    Adding iOS testing target. Change made in November.

---
+++ SwiftSVGiOS/SwiftSVGiOS.h
@@ -0,0 +1,20 @@
+//
+//  SwiftSVGiOS.h
+//  SwiftSVGiOS
+//
+//  Created by Kevin Meaney on 03/11/2015.
+//  Copyright © 2015 No. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftSVGiOS.
+FOUNDATION_EXPORT double SwiftSVGiOSVersionNumber;
+
+//! Project version string for SwiftSVGiOS.
+FOUNDATION_EXPORT const unsigned char SwiftSVGiOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftSVGiOS/PublicHeader.h>
+
+#import <SwiftSVGiOS/MIPathFromSVGPath.h>
+#import <SwiftSVGiOS/MIJSONConstants.h>
